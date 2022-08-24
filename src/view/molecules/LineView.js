c8250f0b956a39a751e6ebf1eba1cd7127ff54d1
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-08-24 13:17:40 +0530

    fix: Render only known Stations

--- src/view/molecules/LineView.js
+++ src/view/molecules/LineView.js
@@ -1,10 +1,12 @@
 import { Polyline } from "react-leaflet";
+
 import { STATION_IDX } from "../../nonview/core/Station";
+
 const STYLE_LINE_POLYLINE = {};
 
 export default function LineView({ line }) {
-  const positions = line.stationNames.map(function (stationName) {
-    return STATION_IDX[stationName].latLng;
-  });
+  const positions = line.stationNames
+    .filter((stationName) => STATION_IDX[stationName])
+    .map((stationName) => STATION_IDX[stationName].latLng);
   return <Polyline pathOptions={STYLE_LINE_POLYLINE} positions={positions} />;
 }
